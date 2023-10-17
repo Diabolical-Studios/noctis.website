c90a0e1b4526e5539e6ee59f3b9a8e9f0922e285
blazittx <info@doruksasmaz.com>
2023-10-17 21:46:43 +0300

    V 0.1 Patreon Auth Error

--- netlify/functions/patreon-auth.js
+++ netlify/functions/patreon-auth.js
@@ -40,6 +40,7 @@ exports.handler = async function (event, context) {
                 }
             };
         } catch (error) {
+            console.error("Error:", error.response.data);
             return {
                 statusCode: 500,
                 body: 'Error authenticating with Patreon.'
