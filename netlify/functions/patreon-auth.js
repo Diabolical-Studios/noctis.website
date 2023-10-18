ff3f639b0bcf496977e34a763ff9c34c115b4f76
blazittx <info@doruksasmaz.com>
2023-10-18 05:20:44 +0300

    Auth Download Button

--- netlify/functions/patreon-auth.js
+++ netlify/functions/patreon-auth.js
@@ -57,31 +57,42 @@ exports.handler = async function (event, context) {
     }
 }
 
-app.get('/check-subscriber-status', async (req, res) => {
-    const accessToken = req.query.accessToken;
-    const isSubscriber = await checkPatreonSubscriber(accessToken);
-    res.send({ isSubscriber });
-});
+// When the page loads
+document.addEventListener('DOMContentLoaded', () => {
+    // Check if we have an accessToken (you'll have to determine how you're passing this back, e.g., in a cookie)
+    const accessToken = getAccessToken(); // You'll need to define this function to retrieve the token
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+    if (accessToken) {
+        checkSubscriptionStatusAndUpdateButton(accessToken);
+    }
 });
 
 async function checkSubscriptionStatusAndUpdateButton(accessToken) {
     const response = await fetch(`/check-subscriber-status?accessToken=${accessToken}`);
     const data = await response.json();
 
-    if (!data.isSubscriber) {
-        const btn = document.querySelector('.header__btn-wrapper');
-        btn.onclick = startDownload;
-        btn.querySelector('.header__btn-text').textContent = 'Download';
+    const btn = document.querySelector('.header__btn');
+    const btnWrapper = btn.querySelector('.header__btn-wrapper');
+    const btnText = btnWrapper.querySelector('.header__btn-text');
+    const svg = btnWrapper.querySelector('svg');
 
-        // Remove Patreon SVG (if you want to)
-        const svg = btn.querySelector('svg');
+    if (!data.isSubscriber) {
+        btn.onclick = startDownload;
+        btnText.textContent = 'Download';
+
+        // Remove Patreon SVG 
         if (svg) svg.remove();
     }
+    // If the user is a subscriber, the button remains unchanged
 }
 
 function startDownload() {
     // Your download logic here...
+    window.location.href = "../assets/images/logo.png";
+}
+
+function getAccessToken() {
+    // Implement the logic to retrieve the accessToken (e.g., from a cookie or session)
+    // This is a placeholder, adjust accordingly
+    return document.cookie.split('; ').find(row => row.startsWith('accessToken=')).split('=')[1];
 }
